50e05bef53497e86546aa5723705e9912bdfcff6
ebnsalemalawal <ebnsalemalawal@gmail.com>
2026-02-28 08:53:09 +0000

    إضافة ملف البصمة الجينية للمشروع

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vdnv7ud6pg"
+    "revision": "0.0qf7arpqa48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
